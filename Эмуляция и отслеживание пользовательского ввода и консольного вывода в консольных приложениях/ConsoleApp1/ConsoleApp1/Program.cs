7f21595159970dfbe0926381239b2c3e53049393
OkuntsovMM2207 <136762720+OkuntsovMM2207@users.noreply.github.com>
2024-06-02 19:45:27 +0700

    Add files via upload

---
+++ Эмуляция и отслеживание пользовательского ввода и консольного вывода в консольных приложениях/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+public class MultiplicationMatrix
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Введите первое число (x):");
+        if (!int.TryParse(Console.ReadLine(), out int x))
+        {
+            Console.WriteLine("Некорректный ввод. Введите целое число.");
+            return;
+        }
+
+        Console.WriteLine("Введите второе число (y):");
+        if (!int.TryParse(Console.ReadLine(), out int y))
+        {
+            Console.WriteLine("Некорректный ввод. Введите целое число.");
+            return;
+        }
+
+        int[,] matrix = new int[y, x];
+
+        for (int i = 0; i < y; i++)
+        {
+            for (int j = 0; j < x; j++)
+            {
+                matrix[i, j] = (i + 1) * (j + 1);
+            }
+        }
+
+        Console.WriteLine("Матрица умножения:");
+        for (int i = 0; i < y; i++)
+        {
+            for (int j = 0; j < x; j++)
+            {
+                Console.Write(matrix[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
