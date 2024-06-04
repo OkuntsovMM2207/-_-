9929714afcd8a30076f6462c7b1689cc4fe80d00
OkuntsovMM2207 <136762720+OkuntsovMM2207@users.noreply.github.com>
2024-06-05 00:50:53 +0700

    Add files via upload

---
+++ Adapter Pattern/ConsoleApp1/ConsoleApp1/ComputerGame.cs
@@ -0,0 +1,23 @@
+﻿internal class ComputerGame
+{
+    private string v1;
+    private object p12;
+    private int v2;
+    private int v3;
+    private int v4;
+    private int v5;
+    private int v6;
+    private double v7;
+
+    public ComputerGame(string v1, object p12, int v2, int v3, int v4, int v5, int v6, double v7)
+    {
+        this.v1 = v1;
+        this.p12 = p12;
+        this.v2 = v2;
+        this.v3 = v3;
+        this.v4 = v4;
+        this.v5 = v5;
+        this.v6 = v6;
+        this.v7 = v7;
+    }
+}
---
+++ Adapter Pattern/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+public class ConsoleApp1
+{
+    public static void Main(string[] args)
+    {
+        ComputerGame game = new ComputerGame("Игра", PegiAgeRating.P12, 60, 4096, 50, 8, 4, 3.5);
+        PCGame pcGame = new ComputerGameAdapter(game);
+
+        Console.WriteLine($"Название игры: {pcGame.getTitle()}");
+        Console.WriteLine($"Возрастное ограничение: {pcGame.getPegiAllowedAge()}");
+        Console.WriteLine($"Triple-A: {pcGame.isTripleAGame()}");
+        Console.WriteLine($"Требования: {pcGame.getRequirements()}");
+    }
+    public class ComputerGameAdapter : PCGame
+    {
+        private ComputerGame computerGame;
+
+        public ComputerGameAdapter(ComputerGame computerGame)
+        {
+            this.computerGame = computerGame;
+        }
+
+        public string getTitle()
+        {
+            return computerGame.getName();
+        }
+
+        public int getPegiAllowedAge()
+        {
+            switch (computerGame.getPegiAgeRating())
+            {
+                case PegiAgeRating.P3:
+                    return 3;
+                case PegiAgeRating.P7:
+                    return 7;
+                case PegiAgeRating.P12:
+                    return 12;
+                case PegiAgeRating.P16:
+                    return 16;
+                case PegiAgeRating.P18:
+                    return 18;
+                default:
+                    return 0;
+            }
+        }
+
+        public bool isTripleAGame()
+        {
+            return computerGame.getBudgetInMillionsOfDollars() > 50;
+        }
+
+        public Requirements getRequirements()
+        {
+            return new Requirements(
+                computerGame.getMinimumGpuMemoryInMegabytes() / 1024,
+                computerGame.getDiskSpaceNeededInGB(),
+                computerGame.getRamNeededInGb(),
+                computerGame.getCoreSpeedInGhz(),
+                computerGame.getCoresNeeded()
+            );
+        }
+    }
+}
